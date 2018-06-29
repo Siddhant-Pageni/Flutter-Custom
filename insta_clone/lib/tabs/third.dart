ada561758330518d42c2b5391262eda61fc91df2
Siddhant Pageni <siddhantpageni@gmail.com>
2018-06-29 23:21:35 +0545

    create profile section

--- insta_clone/lib/tabs/third.dart
+++ insta_clone/lib/tabs/third.dart
@@ -9,7 +9,7 @@ class ThirdTab extends StatelessWidget {
           title: new Container(
             child: new Row(
               children: <Widget>[
-                new Text("sonaam______",
+                new Text("madjack_stark",
                     style: new TextStyle(color: Colors.black)),
                 new Icon(
                   Icons.arrow_drop_down,
@@ -45,11 +45,51 @@ class ThirdTab extends StatelessWidget {
           // Set the background color of the App Bar
           backgroundColor: new Color(0xfff0f0f0)),
       backgroundColor: Colors.white,
+//      body: new Container(
+//        padding: const EdgeInsets.all(8.0),
+//        child: new Row(
+//          children: [
+//            new Container(
+//              width: 80.0,
+//              height: 80.0,
+//              decoration: new BoxDecoration(
+//                  shape: BoxShape.circle,
+//                  image: new DecorationImage(
+//                      image: new NetworkImage(
+//                          "https://instagram.fktm3-1.fna.fbcdn.net/vp/524d1025b2df5ae38329260fe8d42300/5BDF3C7E/t51.2885-19/s150x150/34186137_1061579217351425_3199359905165213696_n.jpg"))),
+//            ),
+//            new Expanded(
+//              child: new Column(
+//                crossAxisAlignment: CrossAxisAlignment.stretch,
+//                children: [
+//                  new Container(
+//                    padding: const EdgeInsets.only(bottom: 8.0),
+//                    child: new Text(
+//                      'Oeschinen Lake Campground',
+//                      style: new TextStyle(
+//                        fontWeight: FontWeight.bold,
+//                      ),
+//                    ),
+//                  ),
+//                  new Text(
+//                    'Kandersteg, Switzerland',
+//                    style: new TextStyle(
+//                      color: Colors.grey[500],
+//                    ),
+//                  ),
+//                ],
+//              ),
+//            ),
+//          ],
+//        ),
+//      ),
+
+
       body: new Container(
         child: new Column(
           children: <Widget>[
             new Padding(
-              padding: new EdgeInsets.all(8.0),
+              padding: new EdgeInsets.all(16.0),
               child: new Row(
                 children: <Widget>[
                   new Container(
@@ -59,41 +99,60 @@ class ThirdTab extends StatelessWidget {
                         shape: BoxShape.circle,
                         image: new DecorationImage(
                             image: new NetworkImage(
-                                "https://instagram.fktm3-1.fna.fbcdn.net/vp/524d1025b2df5ae38329260fe8d42300/5BDF3C7E/t51.2885-19/s150x150/34186137_1061579217351425_3199359905165213696_n.jpg"))),
+                                "https://instagram.fktm3-1.fna.fbcdn.net/vp/167ac9d4422003af0e6ba57882da991e/5BE30E91/t51.2885-19/s150x150/35180230_144248576354865_34214473984114688_n.jpg"))),
                   ),
                   new Expanded(
                     child: new Container(
                       child: new Column(
                         children: <Widget>[
                           new Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
                             children: <Widget>[
                               new Column(
                                 children: <Widget>[
-                                  new Text("test"),
-                                  new Text("test"),
-                                  new Text("test"),
+                                  new Text("64", style: new TextStyle(fontWeight: FontWeight.bold)),
+                                  new Text("posts"),
                                 ],
                               ),
                               new Column(
                                 children: <Widget>[
-                                  new Text("test"),
-                                  new Text("test"),
-                                  new Text("test"),
+                                  new Text("170", style: new TextStyle(fontWeight: FontWeight.bold)),
+                                  new Text("followers"),
                                 ],
                               ),
                               new Column(
                                 children: <Widget>[
-                                  new Text("test"),
-                                  new Text("test"),
-                                  new Text("test"),
+                                  new Text("247", style: new TextStyle(fontWeight: FontWeight.bold)),
+                                  new Text("following"),
                                 ],
                               ),
                             ],
                           ),
+                          new Row(
+                            children: <Widget>[
+                              new FlatButton(onPressed: null, child: new Text("Click Me!"))
+                            ],
+                          )
                         ],
                       ),
                     ),
                   ),
+                ],
+              ),
+            ),
+            new Padding(
+              padding: new EdgeInsets.only(left: 16.0),
+              child: new Row(
+                children: <Widget>[
+                  new Expanded(child: new Container(
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        new Text("Sid Haunt Pageni", style: new TextStyle(fontWeight: FontWeight.bold), textAlign: TextAlign.left),
+                        new Text("🐼🐼🐼🐼\nSoftware Engineer at Inception\nSapiosexual\nGame Lover")
+                      ],
+                    ),
+                  ))
                 ],
               ),
             ),

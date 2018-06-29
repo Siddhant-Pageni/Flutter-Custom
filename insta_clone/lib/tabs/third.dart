e3dbfa5db3a6d4a6158a0f4ffafeebbad725ab7f
Siddhant Pageni <siddhantpageni@gmail.com>
2018-06-29 15:18:38 +0545

    app bar of profile page

--- insta_clone/lib/tabs/third.dart
+++ insta_clone/lib/tabs/third.dart
@@ -3,7 +3,25 @@ import 'package:flutter/material.dart';
 class ThirdTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
+    return new Scaffold(appBar: new AppBar(
+      // Title
+        title: new Container(
+          child: new Row(
+            children: <Widget>[
+              new Text("sonaam______", style: new TextStyle(color: Colors.black)),
+              new Icon(Icons.arrow_drop_down, color: Colors.black,),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.av_timer, color: Colors.black,)),
+          new IconButton(icon: new Icon(Icons.person_add, color: Colors.black,)),
+          new IconButton(icon: new Icon(Icons.more_vert, color: Colors.black,)),
+        ],
+
+        // Set the background color of the App Bar
+        backgroundColor: new Color(0xfff0f0f0)),
+
       backgroundColor: Colors.orange,
       body: new Container(
         child: new Center(

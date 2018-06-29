58f3360640a516799ac40864b151de76d28aebf2
Sonaam <sonaam.hitang@gmail.com>
2018-06-29 15:12:22 +0545

    Add front page

--- insta_clone/lib/tabs/first.dart
+++ insta_clone/lib/tabs/first.dart
@@ -6,12 +6,34 @@ class FirstTab extends StatelessWidget {
     return new Scaffold(
       appBar: new AppBar(
         // Title
-          title:
-          new Text("Instagram", style: TextStyle(fontFamily: 'Billabong')),
-          // Set the background color of the App Bar
-          backgroundColor: Colors.blue,
-          centerTitle: true),
-      backgroundColor: Colors.red,
+        titleSpacing: 8.0,
+        title: new Container(
+          child: new Row(children: <Widget>[
+            new Icon(
+              Icons.camera_alt,
+              color: Colors.black,
+            ),
+            new Container(
+              width: 8.0,
+            ),
+            new Text("Instagram",
+                style: TextStyle(fontFamily: 'Billabong', color: Colors.black)),
+          ]),
+        ),
+//              new Text("Instagram", style: TextStyle(fontFamily: 'Billabong', color: Colors.black)),
+        // Set the background color of the App Bar
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        actions: <Widget>[
+          new IconButton(
+              icon: new Icon(
+                Icons.send,
+                color: Colors.black,
+              ),
+              onPressed: null)
+        ],
+      ),
+      backgroundColor: new Color(0xfff0f0f0),
       body: new Container(
         child: new Center(
           child: new Column(
